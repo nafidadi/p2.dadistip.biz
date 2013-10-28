b347b9216e402e00656d8a74d7668b6ff22f54ca
Dadi <nafi.dadi@gmail.com>
2013-10-28 15:53:07 -0500

    added some files in views and controllers. And also some modified files.

--- config/config.php
+++ config/config.php
@@ -14,11 +14,11 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'Sample App');  
+	define('APP_NAME', 'tellAtale');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
-	define('APP_EMAIL', 'webmaster@sample-app.com'); 
+	define('APP_EMAIL', 'nafidadi@gmail.com'); 
 
 /* 
 A email designated to receive messages from the server. Examples:
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'myapp_com'); 
+	define('DB_NAME', 'p2_dadistip_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
