400717b807eb8f3be312bede02ddd5517b5125eb
Dadi <nafi.dadi@gmail.com>
2013-11-06 10:42:35 -0600

    edited config.php

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p2_dadistip_biz'); 
+	define('DB_NAME', 'dadistip_p2_dadistip_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
