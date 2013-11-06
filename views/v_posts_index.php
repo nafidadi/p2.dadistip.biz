e5498bd777b223a12e2b7283bd8e6e2e2794120a
Dadi <nafi.dadi@gmail.com>
2013-11-06 10:11:50 -0600

    some features added

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -2,12 +2,16 @@
 <section class='posts'>
 	<article>
     		<h4><?php echo $post['first_name']?> <?php echo $post['last_name']?> posted:</h4>
-
+		
     		<time datetime="<?php echo Time::display($post['created'],'Y-m-d G:i')?>">
         		<?php echo Time::display($post['created'])?>
     		</time>
 
     		<p><?php echo $post['content']?></p>
+		
+		<form name='delete' id='del_post' method='POST' action='/posts/p_delete/<?=$post['post_id']?>'>
+			<input type='submit' value='Delete' name='delete' class='submit'>
+		</form>
 		<hr />
 	</article>
 </section>
